b8da711a5d6006cbfbadb0e1b3d6c8cf96a4cedf
Matías Herrera <matias7122@gmail.com>
2021-08-12 22:47:02 -0300

    index.js modificado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //DB en mongoDB Atlas
 mongoose.connect('mongodb+srv://matiash:matiash@carsdb.290x4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -10,11 +10,6 @@ mongoose.connect('mongodb+srv://matiash:matiash@carsdb.290x4.mongodb.net/myFirst
             console.log(`Server runing in port ${PORT}`)
         })
     })
+// app
+//     .get('/', (req, res) => )
 
-//DB en local
-// mongoose.connect('mongodb://localhost:27017/CRUD_database', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         app.listen(PORT, () => {
-//             console.log(`Server runing in port ${PORT}`)
-//         })
-//     })
