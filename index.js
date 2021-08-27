220063f3de222d3947305dbf54caec6500d20d0a
Matías Herrera <matias7122@gmail.com>
2021-08-27 17:04:03 -0300

    added config var Heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 //DB en mongoDB Atlas
-mongoose.connect('mongodb+srv://matiash:matiash@carsdb.290x4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server runing in port ${PORT}`)
